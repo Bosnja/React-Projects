20f5e864dab1bd86157c6c63beb13be4a865cbef
Bosnja <136107488+Bosnja@users.noreply.github.com>
2025-07-08 16:53:17 +0200

    Add files via upload

---
+++ dashboard+tw/src/components/CardItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BadgeDelta, Flex, Card, Metric, Text } from '@tremor/react'
+
+const CardItem = () => {
+  return (
+    <Card className='w-xs' decoration='top' decorationColor='indigo'>
+        <Flex justifyContent='between' alignItems='center'>
+           <Text>Sales</Text> 
+           <BadgeDelta deltaType='moderateIncrease'>+12.5%</BadgeDelta>
+        </Flex>
+        
+        <Metric>$ 34,743</Metric>
+    </Card>
+  )
+}
+
+export default CardItem
